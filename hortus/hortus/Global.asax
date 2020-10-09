e39998888e3a3df13771ac2cf1417888c7ff0fc2
Matheus Silva Rego <matheus.gomes@mindsatwork.com.br>
2020-10-08 22:43:22 -0300

    Creating back-end base

---
+++ hortus/hortus/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hortus.MvcApplication" Language="C#" %>
---
+++ hortus/hortus/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+
+namespace hortus.Models
+{
+    public class UserModel
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private string email { get; set; }
+        private string password { get; set; }
+    }
+}
