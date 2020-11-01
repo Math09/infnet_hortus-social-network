4ef29014350e485be6b8018a060d108a34c6283f
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2020-11-01 17:42:14 -0300

    Creating scripts

---
+++ sql/table_post.sql
@@ -0,0 +1,15 @@
+﻿BEGIN TRANSACTION;
+
+	CREATE TABLE post(
+		
+		id_post INT IDENTITY PRIMARY KEY NOT NULL,
+		id_user INT NOT NULL,
+		CONSTRAINT fk_post_user FOREIGN KEY (id_user) REFERENCES dbo.users (id_user),
+
+		photo VARCHAR(255) NULL,
+		description_post VARCHAR(255) NULL,
+		like_post INT NULL,
+
+	);
+
+COMMIT;
